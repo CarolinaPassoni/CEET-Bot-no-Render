d035111e51dcb45e1d0969fff304094783212be5
Carolina Passoni <105358062+CarolinaPassoni@users.noreply.github.com>
2026-09-10 17:55:58 -0300

    Update bootstrap.php

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
+
+if (ob_get_level() === 0) {
+    ob_start();
+}
+
 define('APP_ROOT', dirname(__DIR__));
+
 $config = require APP_ROOT . '/config.php';
+
 require_once APP_ROOT . '/includes/db.php';
 require_once APP_ROOT . '/includes/functions.php';
